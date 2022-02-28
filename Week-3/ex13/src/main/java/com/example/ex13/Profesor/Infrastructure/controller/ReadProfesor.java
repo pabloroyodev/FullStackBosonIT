a85f2c9748d4b6db53664d98d65a22044ff02ee7
[Pablo Royo] <pabloroyodev@gmail.com>
2022-02-28 21:34:08 +0100

    Update ReadProfesor.java

--- Week-3/ex13/src/main/java/com/example/ex13/Profesor/Infrastructure/controller/ReadProfesor.java
+++ Week-3/ex13/src/main/java/com/example/ex13/Profesor/Infrastructure/controller/ReadProfesor.java
@@ -1,4 +1,28 @@
 package com.example.ex13.Profesor.Infrastructure.controller;
 
+import com.example.ex13.Profesor.Application.ProfesorService;
+import com.example.ex13.Profesor.Infrastructure.controller.dto.output.ProfesorOutputDto;
+import com.example.ex13.Student.Infrastructure.controller.dto.output.StudentOutputDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("profesor")
+@RestController
 public class ReadProfesor {
+
+    @Autowired
+    ProfesorService profesorService;
+
+    @GetMapping
+    public List<ProfesorOutputDto> findAll(){
+        return profesorService.getAllProfesores();
+    }
+
+    @GetMapping("{id}")
+    public ProfesorOutputDto getProfesorById(@PathVariable Integer id) throws Exception {
+        return profesorService.filterProfesorById(id);
+    }
 }
