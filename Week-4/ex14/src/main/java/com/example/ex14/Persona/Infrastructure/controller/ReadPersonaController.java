2cb9a4d82173dc3961ad51b7e24fdef930e44661
[Pablo Royo] <pabloroyodev@gmail.com>
2022-03-02 15:00:29 +0100

    Feing

--- Week-4/ex14/src/main/java/com/example/ex14/Persona/Infrastructure/controller/ReadPersonaController.java
+++ Week-4/ex14/src/main/java/com/example/ex14/Persona/Infrastructure/controller/ReadPersonaController.java
@@ -2,11 +2,15 @@ package com.example.ex14.Persona.Infrastructure.controller;
 
 import com.example.ex14.Persona.Application.PersonaService;
 import com.example.ex14.Persona.Infrastructure.controller.dto.output.PersonaOutputDto;
+import com.example.ex14.Profesor.Infrastructure.controller.dto.output.ProfesorOutputDto;
+import com.example.ex14.utils.Feing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -16,6 +20,9 @@ public class ReadPersonaController {
 
     @Autowired
     PersonaService personaService;
+
+    @Autowired
+    Feing feign;
 
     @GetMapping
     public List<PersonaOutputDto> findAll(){
@@ -30,5 +37,17 @@ public class ReadPersonaController {
     @GetMapping("/{usuario}/usuario")
     public List<PersonaOutputDto> getPersonaByUsuario(@PathVariable String usuario){
         return personaService.filterPersonaByUser(usuario);
+    }
+
+    @GetMapping("/profesor/{id}")
+    ResponseEntity<ProfesorOutputDto> getProfesorRestTemplate(@PathVariable Integer id){
+        ResponseEntity<ProfesorOutputDto> rs = new RestTemplate().getForEntity("http://localhost:8081/profesor/"+id,ProfesorOutputDto.class);
+        return ResponseEntity.ok(rs.getBody());
+
+    }
+    @GetMapping("/feing/{id}")
+    ResponseEntity<ProfesorOutputDto> getProfesorFeign(@PathVariable Integer id){
+        ResponseEntity<ProfesorOutputDto> rs = feign.callServer(id);
+        return rs;
     }
 }
