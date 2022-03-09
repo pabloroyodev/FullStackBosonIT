d0a17cdabff0d2d94aa6167072919a47ace69b6f
[Pablo Royo] <pabloroyodev@gmail.com>
2022-03-09 15:01:54 +0100

    3. Display a selection list

--- Week-5/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Week-5/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,15 +8,15 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero; //No habra ningun hero seleccionado por defecto.
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
