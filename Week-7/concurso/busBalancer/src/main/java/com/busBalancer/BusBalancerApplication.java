3ef01397cba82369f7a48e6e994eba41ac1b8380
[Pablo Royo] <pabloroyodev@gmail.com>
2022-04-11 19:05:58 +0200

    Gateway

---
+++ Week-7/concurso/busBalancer/src/main/java/com/busBalancer/BusBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.busBalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBalancerApplication.class, args);
+	}
+
+}
---
+++ Week-7/concurso/busBalancer/src/test/java/com/busBalancer/BusBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
